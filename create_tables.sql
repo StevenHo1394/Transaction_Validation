c5d7b0acfebad1205fe49ed8c618ce871e01ecc7
steven <stevnho1394@gmail.com>
2021-11-22 23:13:04 +0800

    modified "create_table.sql"

--- create_tables.sql
+++ create_tables.sql
@@ -1,27 +1,26 @@
-DROP TABLE If EXISTS Transactions;
-CREATE TABLE Transactions
-(
-  id int NOT NULL auto_increment 
-	primary key, 
-  discord_id VARCHAR(255),
-  transaction_hash VARCHAR(255),
-  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  transaction_type_id VARCHAR(255)
-	foreign key REFERENCES TransactionTypes(tid),
-  constraint Transactions_id_uindex
-	unique (id)
-)
-ENGINE = InnoDB;
-
 DROP TABLE If EXISTS TransactionTypes;
 CREATE TABLE TransactionTypes
 (
   tid int NOT NULL auto_increment
-	primary key, 
+	primary key,
   name VARCHAR(255),
   transaction_hash VARCHAR(255),
   description VARCHAR(255),
-  amount DOUBLE(20)
+  amount DOUBLE
+)
+ENGINE = InnoDB;
+
+DROP TABLE If EXISTS Transactions;
+CREATE TABLE Transactions
+(
+  id int NOT NULL auto_increment
+	primary key,
+  discord_id VARCHAR(255),
+  transaction_hash VARCHAR(255),
+  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  tid int NOT NULL,
+  foreign key (tid) REFERENCES TransactionTypes(tid),
+  constraint Txn_id_uindex unique (id)
 )
 ENGINE = InnoDB;
 
